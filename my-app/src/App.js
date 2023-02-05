455030167a43ff52b00b359ee2eac9c96aa08485
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-05 20:52:04 +0000

    Handling Events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import css from "./App.module.css";
-import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForms"
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
+      <NavBarForm />
       <Sidebar />
     </div>
   );
