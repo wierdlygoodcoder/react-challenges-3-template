f20175801557384228de903c13a1803046ef105a
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-05 21:19:00 +0000

    Rendering Lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import css from "./App.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForms"
+import Content from "./components/Content";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
